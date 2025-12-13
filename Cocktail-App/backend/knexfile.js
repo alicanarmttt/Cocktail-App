c56d68df2077bf81ad7888208740b2d26be5cb79
alicanarmttt <131194727+alicanarmttt@users.noreply.github.com>
2025-12-13 15:35:53 +0300

    knexfile and supabase connection edit

--- Cocktail-App/backend/knexfile.js
+++ Cocktail-App/backend/knexfile.js
@@ -15,20 +15,11 @@ module.exports = {
   production: {
     client: "pg",
     connection: {
-      // URL yerine PARÇALI YAPI kullanıyoruz.
-      // Bu sayede alttaki family: 4 ayarı KESİN çalışacak.
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT) || 6543,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
+      connectionString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false },
-
-      // İŞTE BÜYÜLÜ SATIR BURADA
-      family: 4,
     },
+    pool: { min: 2, max: 10 },
     migrations: { directory: "./src/db/migrations" },
     seeds: { directory: "./src/db/seeds" },
-    pool: { min: 2, max: 10 },
   },
 };
