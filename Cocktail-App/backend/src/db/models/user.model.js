61efc9db94e2cd5b22f5ba56c76add5a32096168
alicanarmttt <131194727+alicanarmttt@users.noreply.github.com>
2025-12-15 18:54:24 +0300

    User.model knexfile bug fixed

--- Cocktail-App/backend/src/db/models/user.model.js
+++ Cocktail-App/backend/src/db/models/user.model.js
@@ -1,3 +1,4 @@
+const knexFile = require("../../../knexfile.js");
 const environment = process.env.NODE_ENV || "development";
 const knexConfig = knexFile[environment];
 const db = require("knex")(knexConfig);
